cb076b01a68a1b4e9845f794ca6d3b2d94d6b837
xdkillyou <xdkillyou@gmail.com>
2024-01-06 21:50:46 +0800

    feat: add flash animate to squareBox

--- src/components/squareBox.vue
+++ src/components/squareBox.vue
@@ -1,8 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  index: {
+    type: Number,
+    required: true,
+  },
+});
+
+const isFlashBox = computed(() => {
+  switch (props.index) {
+    case 3:
+    case 5:
+    case 9:
+      return true;
+    default:
+      return false;
+  }
+});
+</script>
 
 <template>
   <!-- bg-[radial-gradient(circle,rgba(113,81,95,1)_81%,rgba(0,0,0,1)_100%)] -->
-  <div class="h-100px border-black border-2px squareBoxBg"></div>
+  <div class="border-black border-2px">
+    <div
+      :class="isFlashBox ? 'animate-pulse' : ''"
+      class="h-100px squareBoxBg"
+    ></div>
+  </div>
 </template>
 
 <style>

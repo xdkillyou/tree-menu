de0e01a436cf5f8b6c1bdbae0860f3fd253156c8
xdkillyou <xdkillyou@gmail.com>
2024-01-06 21:49:49 +0800

    feat: add header & squareBox to Main view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-const a = '123';
+import SquareBox from '@/components/squareBox.vue';
+import Header from '@/components/header/headerContent.vue';
 </script>
 
 <template>
-  <div>123</div>
+  <div>
+    <Header />
+    <div class="fixed top-38px right-0 left-0 bottom-0 bg-gray/40">
+      <div class="absolute top-1/2 transform -translate-y-1/2 w-full">
+        <div class="grid grid-cols-3 gap-2 mx-4px">
+          <SquareBox v-for="index in 9" :key="index" :index="index" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style></style>

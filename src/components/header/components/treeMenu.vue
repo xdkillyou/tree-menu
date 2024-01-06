5bae3166a40ee522eaf31ced24b66ddf96b1875a
xdkillyou <xdkillyou@gmail.com>
2024-01-06 16:17:54 +0800

    feat: add header & tree menu component

---
+++ src/components/header/components/treeMenu.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const isTreeMenuShow = ref(false);
+function show() {
+  isTreeMenuShow.value = true;
+}
+function hide() {
+  isTreeMenuShow.value = false;
+}
+
+defineExpose({
+  isTreeMenuShow,
+  show,
+  hide,
+});
+</script>
+
+<template>
+  <!-- mask -->
+  <div
+    v-show="isTreeMenuShow"
+    @click="hide"
+    class="absolute top-0 left-0 right-0 bottom-0"
+  ></div>
+
+  <Transition>
+    <div
+      v-show="isTreeMenuShow"
+      class="absolute h-100vh w-300px bg-black text-white"
+    >
+      <div>123</div>
+    </div>
+  </Transition>
+</template>
---
+++ src/components/header/headerContent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TreeMenu from '@/components/header/components/treeMenu.vue';
+
+const treeMenuRef = ref();
+</script>
+
+<template>
+  <div class="flex justify-end">
+    <button
+      @click="treeMenuRef.show()"
+      type="button"
+      class="border border-black/60 bg-gray/20 rounded-4px px-12px py-2px m-4px hover:bg-gray/30"
+    >
+      三
+    </button>
+
+    <TreeMenu ref="treeMenuRef" />
+  </div>
+</template>

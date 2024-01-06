173ae8c158772bc461f88a11dbabfe98e4a9f47d
xdkillyou <xdkillyou@gmail.com>
2024-01-06 15:37:13 +0800

    feat: add square box component

---
+++ src/components/squareBox.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- bg-[radial-gradient(circle,rgba(113,81,95,1)_81%,rgba(0,0,0,1)_100%)] -->
+  <div class="h-100px border-black border-2px squareBoxBg"></div>
+</template>
+
+<style>
+.squareBoxBg {
+  background: radial-gradient(
+    circle,
+    rgba(113, 81, 95, 1) 81%,
+    rgba(0, 0, 0, 1) 100%
+  );
+}
+</style>

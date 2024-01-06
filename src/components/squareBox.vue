0cfda69bace715fc7144d410fbd48b11e9c00c85
xdkillyou <xdkillyou@gmail.com>
2024-01-07 01:21:48 +0800

    feat: add ball & animate

--- src/components/squareBox.vue
+++ src/components/squareBox.vue
@@ -16,15 +16,33 @@ const isFlashBox = computed(() => {
       return false;
   }
 });
+
+const haveBallBox = computed(() => {
+  switch (props.index) {
+    case 1:
+    case 3:
+    case 7:
+    case 9:
+      return true;
+    default:
+      return false;
+  }
+});
 </script>
 
 <template>
   <!-- bg-[radial-gradient(circle,rgba(113,81,95,1)_81%,rgba(0,0,0,1)_100%)] -->
-  <div class="border-black border-2px">
+  <div class="border-black border-2px relative">
     <div
-      :class="isFlashBox ? 'animate-pulse' : ''"
+      :class="isFlashBox ? 'animate-flash' : ''"
       class="h-100px squareBoxBg"
     ></div>
+    <div
+      v-if="haveBallBox"
+      class="animate-slide absolute z-1 top-1/2 left-1/2 rounded-full bg-#A5F12B w-30px h-30px text-center leading-30px"
+    >
+      0
+    </div>
   </div>
 </template>
 
--- uno.config.ts
+++ uno.config.ts
@@ -6,20 +6,29 @@ export default defineConfig({
     animation: {
       keyframes: {
         flash: `{
-          0%,
-          100% {
+          0%,100% {
             opacity: 1;
           }
           50% {
             opacity: 0.6;
           }
         }`,
+        slide: `{
+          0% {
+            transform: translateX(0px) translateY(-50%);
+          }
+          100% {
+            transform: translateX(300px) translateY(-50%);
+          }
+        }`,
       },
       durations: {
         flash: '1s',
+        slide: '1.5s',
       },
       counts: {
         flash: 'infinite',
+        slide: 'infinite',
       },
     },
   },

e2b23f4bf1b10fcea934d38043c7e283a7af06be
xdkillyou <xdkillyou@gmail.com>
2024-01-07 17:08:29 +0800

    feat: add mock data & interface

---
+++ src/interface/data.ts
@@ -0,0 +1,13 @@
+export interface IMenuItemRawData {
+  key: string;
+  text: string;
+  children?: IMenuItemRawData[];
+}
+
+export interface IMenuItem {
+  key: string;
+  text: string;
+  expand: boolean;
+  path: string;
+  children?: IMenuItem[];
+}

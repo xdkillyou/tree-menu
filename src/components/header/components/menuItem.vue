4df9216b01a5615f9337fb69badbe6de09559def
xdkillyou <xdkillyou@gmail.com>
2024-01-08 00:32:46 +0800

    feat: create menu item and recursive component

---
+++ src/components/header/components/menuItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { type IMenuItem } from '@/interface/data';
+import MenuItem from './menuItem.vue';
+
+defineProps({
+  item: {
+    type: Object as PropType<IMenuItem>,
+    default: () => {},
+  },
+  children: {
+    type: Array as PropType<IMenuItem[]>,
+    default: () => [],
+  },
+});
+
+function expandItem(item: IMenuItem) {
+  item.expand = !item.expand;
+}
+
+const expandEvent = inject('expandEvent');
+</script>
+
+<template>
+  <div :class="item.expand ? 'bg-gray-600' : ''">
+    <div
+      @click="() => expandEvent(item.path)"
+      class="px-15px py-10px"
+      :class="item.expand ? 'text-yellow' : ''"
+    >
+      {{ item.text }}
+    </div>
+
+    <MenuItem
+      v-show="children.length > 0 && item.expand"
+      v-for="childrenItem in children"
+      :key="childrenItem.key"
+      :item="childrenItem"
+      :children="childrenItem.children"
+      class="pl-15px"
+    />
+  </div>
+</template>
--- src/components/header/components/treeMenu.vue
+++ src/components/header/components/treeMenu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { type IMenuItem } from '@/interface/data';
+import MenuItem from './menuItem.vue';
 
 const props = defineProps({
   data: {
@@ -34,9 +35,14 @@ defineExpose({
   <Transition name="slide">
     <div
       v-show="isTreeMenuShow"
-      class="fixed top-0 bottom-0 w-200px bg-black text-white z-1"
+      class="fixed top-0 bottom-0 w-200px bg-black text-white z-1 overflow-y-scroll"
     >
-      <div>123</div>
+      <MenuItem
+        v-for="item in props.data"
+        :key="item.key"
+        :item="item"
+        :children="item.children"
+      />
     </div>
   </Transition>
 </template>
